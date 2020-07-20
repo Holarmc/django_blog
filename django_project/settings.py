d700907392f29baca0f38ca735f4e0170de38aeb
mcdavies <agboolamicheal95@gmail.com>
2020-07-20 02:31:45 +0100

    New_env, rename 'src', install decouple and secure settings, migrate.
    -Changed the virtualenv frim env to djblog-env
    -Installed decouple to secure settings and created .env file
    -Renamed 'src' to 'django_project
    -Removed sq.dbelite from .gitignore
    -local server return blog_post does not exit error
    -Make migrations to repopulate database

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v-x@%h&_y@c*$#e^#a$1qytlh-5a!-@x(h_gjv69xh))&jo&f8'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': config('DB_NAME'),
     }
 }
 
